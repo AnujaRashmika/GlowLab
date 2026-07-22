212934eedf7a0d06d84a74619807e79b687283f8
Anuja005 <168641152+Anuja005@users.noreply.github.com>
2026-07-22 21:14:28 +0530

    create purple_bg.xml and Splash Screen. it set as launcher activity. now Splash Screen is completed

--- app/src/main/java/lk/kotlin/glowlab/SplashActivity.kt
+++ app/src/main/java/lk/kotlin/glowlab/SplashActivity.kt
@@ -1,20 +1,25 @@
 package lk.kotlin.glowlab
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import lk.kotlin.glowlab.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            startBtn.setOnClickListener {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            }
         }
     }
 }
